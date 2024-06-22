bc6a6aa214788d51514f400d37ae05963992bf0a
Vihaan Sondhi <vihaansondhi07@gmail.com>
2024-06-22 13:27:55 -0400

    Quartz sync: Jun 22, 2024, 1:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: " Vihaan",
+    pageTitle: "Vihaan",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
